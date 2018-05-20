1042d34b3730d850ce7a4f499330a71e2b7e1b88
Mike Sale <2355139+mikesale@users.noreply.github.com>
2018-05-20 10:39:21 -0600

    initial add of github clone with ssh format.

---
+++ cheatsheets/GitNext.rb
@@ -0,0 +1,20 @@
+cheatsheet do
+  title 'Git Next'
+  docset_file_name 'Git Next'
+  keyword 'gitnext'
+  source_url 'http://mikes.online'
+
+category do
+	id 'Connect'
+
+	entry do
+		name 'Clone github from cli'
+		notes <<-'END'
+			```github
+			git clone git@github.com:username/repos.git
+			```
+		END
+	end
+end
+notes 'Use this sheet to build up memorized commands, then switch out when ready for the next level!'
+end
