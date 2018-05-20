624a0e9b09449e84fe71df937c5af416b6a7ab82
Mike Sale <2355139+mikesale@users.noreply.github.com>
2018-05-20 11:07:52 -0600

    :art: Basic text formating
    bullet out list and set code around just the code

--- cheatsheets/iWork_Numbers.rb
+++ cheatsheets/iWork_Numbers.rb
@@ -9,16 +9,17 @@ cheatsheet do
     entry do
       name 'Mock Simple Pivot Table'
       notes <<-'END'
-        ``` numbers
         Assuming the tables are named Original and Pivot, the cells in Pivot are filled using this formula:
-
-        =SUMIFS(Original::$C, Original::$A, "="& $A2, Original::$B, "="& B$1)
-        i.e. sum values in Original::$C if
-
-        corresponding value in Original::$A = value in first column of Pivot
-        corresponding value in  rOriginal::$B = value in first row of Pivot
-        The 'all' values are just simple sums of those rows or columns.
         ```
+        =SUMIFS(Original::$C, Original::$A, "="& $A2, Original::$B, "="& B$1)
+        ```
+        
+        
+        i.e. sum values in Original::$C if
+        
+        * corresponding value in Original::$A = value in first column of Pivot
+        * corresponding value in  rOriginal::$B = value in first row of Pivot
+        The 'all' values are just simple sums of those rows or columns.
       END
     end
   end
